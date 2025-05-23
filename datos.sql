ea80d7475852fb0f441b6b9d8e5bd238d46c40be
sebasBetancourt <s3basbetan@gmail.com>
2025-05-23 12:11:32 -0500

    feat: :art: Add Dates of structure

---
+++ datos.sql
@@ -0,0 +1,8 @@
+use rrhh_agencia;
+
+INSERT INTO empleados(nombre, apellido, correo)VALUES
+("Sebastian", "Betancourt", "s3basbetan@gmail.com"),
+("Alejandro", "Naranja", "alejoxd1234@gmail.com"),
+("TristePe", "Trigopan", "danielzurfer123@gmail.com"),
+("Nicolas", "Angarita", "nicotur@gmail.com"),
+("Bastian", "Nova", "bastian2211@ŋmail.com");
